b99491c051b9b80b11f013fb3e00c6d9f070034c
jiho lee <jh001007@gmail.com>
2019-06-27 22:51:52 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const optionDefinitions = [
     { name: 'link', alias: 'l', type: String, multiple: true, defaultOption: true },
     { name: 'help', alias: 'h', type: Boolean},
     { name: 'path', alias: 'p', type: String }
-]
+] 
 
 
 const fs = require('fs');
@@ -18,31 +18,31 @@ if (options['help'] == true) {
     console.log("node main.js -p PATH(optional) -l youtube_link(s)");
 }
 
-let link = options['link'];
+let link = options['link']; // array of links
 
-let path = options['path'] || "./video/";
+let path = options['path'] || "./video/"; // optional
 
 fs.existsSync(path) || fs.mkdirSync(path);
 
 let index = 0;
 let size = link.length;
 
-link.forEach(video => {
-    ytdl.getInfo(video, (err, info) => {
+link.forEach(video => { // video link forEach
+    ytdl.getInfo(video, (err, info) => { // getting info Map
         if (err) throw err;
         index += 1;
         console.log("downloading " + info['title'] + " as " + info['video_id'] + "!");
-        ytdl(video, { quality: 137 }).pipe(fs.createWriteStream(path + info['video_id'] + '_videoonly.mp4')).on("finish", ()=> {
-            ytdl(video, { quality: 140 }).pipe(fs.createWriteStream(path + info['video_id'] + '_audioonly.m4a')).on("finish", ()=>{
-                ffmpeg()
+        ytdl(video, { quality: 137 }).pipe(fs.createWriteStream(path + info['video_id'] + '_videoonly.mp4')).on("finish", ()=> { // download video only file. when it's finished
+            ytdl(video, { quality: 140 }).pipe(fs.createWriteStream(path + info['video_id'] + '_audioonly.m4a')).on("finish", ()=>{ // download audio only file
+                ffmpeg() // then use ffmpeg
                     .mergeAdd(path + info['video_id'] + '_videoonly.mp4')
-                    .mergeAdd(path + info['video_id'] + '_audioonly.m4a')
+                    .mergeAdd(path + info['video_id'] + '_audioonly.m4a') // wrap video to audio
                     .on("end", ()=>{
-                        fs.unlinkSync(path + info['video_id'] + '_videoonly.mp4');
-                        fs.unlinkSync(path + info['video_id'] + '_audioonly.m4a');
+                        fs.unlinkSync(path + info['video_id'] + '_videoonly.mp4'); // delete video only file
+                        fs.unlinkSync(path + info['video_id'] + '_audioonly.m4a'); // delete audio only file
                         console.log(index + "/" + size + " complete!");
                     })
-                    .save(path + info['video_id'] + '.mp4');
+                    .save(path + info['video_id'] + '.mp4'); // finally save
             })
         })
     });
